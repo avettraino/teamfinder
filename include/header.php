06f2f851ead022529cc95aff8d2b9d742dec7345
Tiffy775 <floriandetoffol@yahoo.fr>
2015-04-12 17:08:13 +0200

    anim logo

--- include/header.php
+++ include/header.php
@@ -1,8 +1,10 @@
 <header>
 <!--<img src="image/ville2.JPG" id="ville"/>-->
-<p class="titre_header">TeamFinder</p>
-<img src="image/titre.JPG" id="logo"/>
+<img src="image/img1.JPG" class="logo" id="logo1"/>
 
+<p class="titre_header">TeamFinder</p>
+
+<img src="image/img2.JPG" class="logo" id="logo2"/>
 <!--<p class="slogan">Find a Team</p>-->
 
 </header>
--- include/main_menu.php
+++ include/main_menu.php
@@ -4,7 +4,6 @@
 	<li><a href="inscription.php">Inscription</a></li>
 	<!--<li><a href="">Connexion</a></li>-->
 	<li><a href="">Les demandes</a></li>
-	<li><a href="">Les DaysPlan</a></li>
 	<li><a href="">Photos et avis</a></li>
 	<li><a href="deconnexion.php">Deconnexion</a></li>
 </ul>
